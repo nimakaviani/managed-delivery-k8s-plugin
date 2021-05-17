4b139277bf73bf636cf003a70ecf15e6a76e48c1
Nima Kaviani <17132353+nimakaviani@users.noreply.github.com>
2021-05-17 13:12:36 -0700

    refactor helm resources (#31)
    - allow nullable apiVersion and Kind
    - add tests
    - add verifications

--- k8s-plugin/src/main/kotlin/com/amazon/spinnaker/keel/k8s/resolver/HelmResourceHandler.kt
+++ k8s-plugin/src/main/kotlin/com/amazon/spinnaker/keel/k8s/resolver/HelmResourceHandler.kt
@@ -1,6 +1,7 @@
 package com.amazon.spinnaker.keel.k8s.resolver
 
 import com.amazon.spinnaker.keel.k8s.*
+import com.amazon.spinnaker.keel.k8s.exception.MisconfiguredObjectException
 import com.amazon.spinnaker.keel.k8s.model.HelmResourceSpec
 import com.amazon.spinnaker.keel.k8s.service.CloudDriverK8sService
 import com.fasterxml.jackson.module.kotlin.jacksonObjectMapper
@@ -22,6 +23,28 @@ class HelmResourceHandler(
     cloudDriverK8sService, taskLauncher, eventPublisher, orcaService, resolvers
 ) {
     override val supportedKind = HELM_RESOURCE_SPEC_V1
+
+    public override suspend fun toResolvedType(resource: Resource<HelmResourceSpec>): K8sObjectManifest {
+
+        if (resource.spec.template.apiVersion == FLUX_HELM_API_VERSION && resource.spec.template.kind == FLUX_HELM_KIND) {
+            return super.toResolvedType(resource)
+        }
+
+        // verify if passed in values are right
+        mapOf(
+            "apiVersion" to mapOf("actual" to resource.spec.template.apiVersion, "expected" to FLUX_HELM_API_VERSION),
+            "kind" to mapOf("actual" to resource.spec.template.kind, "expected" to FLUX_HELM_KIND)
+        ).forEach{(key, values) ->
+            if (values["actual"] != null) throw MisconfiguredObjectException(key, values["actual"]!!, values["expected"]!!)
+        }
+
+        return K8sObjectManifest(
+            FLUX_HELM_API_VERSION,
+            FLUX_HELM_KIND,
+            resource.spec.template.metadata,
+            resource.spec.template.spec
+        )
+    }
 
     override suspend fun current(resource: Resource<HelmResourceSpec>): K8sObjectManifest? {
         val clusterResource = cloudDriverK8sService.getK8sResource(resource) ?: return null
